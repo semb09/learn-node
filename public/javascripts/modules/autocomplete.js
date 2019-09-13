8acff9033242428a5e2abc4d6b213c31d45cee92
Stephen Embleton <stephen@wearesurge.co>
2019-09-13 11:44:30 +0100

    complete course

---
+++ public/javascripts/modules/autocomplete.js
@@ -0,0 +1,18 @@
+function autocomplete(input, latInput, lngInput) {
+  if (!input) return;
+  const dropdown = new google.maps.places.Autocomplete(input);
+
+  dropdown.addListener('place_changed', () => {
+    const place = dropdown.getPlace();
+    if (!place) return;
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  });
+
+  // if someone hits enter on the address field, dont submit the form
+  input.on('keydown', e => {
+    if (e.keyCode === 13) e.preventDefault();
+  })
+}
+
+export default autocomplete;
